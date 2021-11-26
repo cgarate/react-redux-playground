ff1a18007f35916505c904de4622fe48e12240e6
Carlos Garate <carlos.m.garate@gmail.com>
2021-11-26 11:25:29 -0500

    setup basic components, initial store

---
+++ src/components/Horoscope.jsx
@@ -0,0 +1,17 @@
+// Displays the user's horoscope
+
+import React from "react";
+
+const Horoscope = () => {
+  return (
+    <div>
+      <header>Your Daily Horoscope</header>
+      <div>
+        <h3>Sign Name</h3>
+        <p>Horoscope</p>
+      </div>
+    </div>
+  );
+};
+
+export default Horoscope;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,16 @@
+// Displays the user's location weather
+import React from "react";
+
+const Weather = () => {
+  return (
+    <div>
+      <header>Your Local Weather</header>
+      <div>
+        <h3>Location Name</h3>
+        <p>Weather details</p>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
